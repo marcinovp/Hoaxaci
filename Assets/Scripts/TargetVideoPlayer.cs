41fab9eff93493d8e532839d82541324a3cc1e55
psych0 <p.marcinov@gmail.com>
2019-12-16 21:35:50 +0100

    Skripty a prefaby frameworku

---
+++ Assets/Scripts/TargetVideoPlayer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TargetVideoPlayer : MonoBehaviour
+{
+    public ImageTargetExtended imageTargetBehaviour;
+    public float startFromTime = 0f;
+
+    public VideoPlayer VideoPlayer { get; private set; }
+    private MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        //Debug.Log("Awake");
+        VideoPlayer = GetComponent<VideoPlayer>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+    
+    private void Start()
+    {
+        //Debug.Log("Start");
+        VideoPlayer.time = startFromTime;
+        VideoPlayer.prepareCompleted += VideoPlayer_prepareCompleted;
+        imageTargetBehaviour.TargetFound += ImageTargetBehaviour_TargetFound;
+        imageTargetBehaviour.TargetLost += ImageTargetBehaviour_TargetLost;
+    }
+
+    private void VideoPlayer_prepareCompleted(VideoPlayer source)
+    {
+        Debug.Log("VideoPlayer_prepareCompleted");
+
+        meshRenderer.enabled = true;
+    }
+    
+    private void ImageTargetBehaviour_TargetLost(ImageTargetController obj)
+    {
+        Debug.Log("Target lost");
+        VideoPlayer?.Pause();
+        meshRenderer.enabled = false;
+    }
+
+    private void ImageTargetBehaviour_TargetFound(ImageTargetController obj)
+    {
+        Debug.Log("Target found");
+        VideoPlayer.Play();
+
+        if (VideoPlayer.isPrepared)
+        {
+            meshRenderer.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoManager : MonoBehaviour
+{
+    public List<ImageTargetExtended> imageTargetBehaviour;
+    public List<TargetVideoPlayer> targetVideoPlayers;
+
+    void Start()
+    {
+        for (int i = 0; i < imageTargetBehaviour.Count; i++)
+        {
+            int index = i;
+            imageTargetBehaviour[i].TargetFound += (x) => Target_TargetFound(imageTargetBehaviour[index]);
+            //videos[i].imageTargetBehaviour.TargetLost += (x) => Target_TargetLost(index);
+        }
+    }
+
+    private void Target_TargetFound(ImageTargetController imageTargetFound)
+    {
+        foreach (var targetVideoPlayer in targetVideoPlayers)
+        {
+            if (targetVideoPlayer.imageTargetBehaviour != imageTargetFound)
+            {
+                targetVideoPlayer.VideoPlayer.Stop();
+            }
+        }
+    }
+
+    private void Target_TargetLost(int index)
+    {
+        
+    }
+
+    private IEnumerator DelayAction(float delaySeconds, Action action)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        action();
+    }
+}
